847b3ea0f7e7417cb32191ef4864a506c903a19f
mongsam2 <songjaehyun121476@gmail.com>
2025-02-01 03:48:20 +0900

    feat: student_records 모델들 정의, 어드민 페이지 등록

--- student_records/admin.py
+++ student_records/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import StudentRecord, Summarization
+from common.admin import CommonAdmin
 
 # Register your models here.
+@admin.register(StudentRecord)
+class StudentRecordAdmin(CommonAdmin):
+    pass
--- student_records/models.py
+++ student_records/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from common.models import CommonDocument
 
 # Create your models here.
+class StudentRecord(CommonDocument):
+    summarization = models.OneToOneField('Summarization', on_delete=models.SET_NULL, null=True, blank=True, related_name='student_record')
+
+class Summarization(models.Model):
+    content = models.TextField()
+    question = models.TextField()
+
+    def __str__(self):
+        return f"{self.student_record.student}의 생기부 요약문"
