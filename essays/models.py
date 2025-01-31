21899ce6e3cd9795040f2bacd7bc1dbf8ec0c766
mongsam2 <songjaehyun121476@gmail.com>
2025-02-01 04:01:50 +0900

    feat: essays 모델들 생성, 어드민 등록

--- essays/models.py
+++ essays/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from common.models import CommonDocument
 
 # Create your models here.
+class Essay(CommonDocument):
+    '''
+    논술 답안지를 저장하는 모델
+    '''
+    evaluation = models.OneToOneField('Evaluation', on_delete=models.SET_NULL, null=True, blank=True)
+    criteria = models.ForeignKey('EssayCriteria', on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class Evaluation(models.Model):
+    content = models.TextField()
+
+
+class EssayCriteria(models.Model):
+    '''
+    논술의 평가 항목과 글자 수 범위에 따른 패널티를 저장하는 모델
+    '''
+    content = models.TextField()
+    ranges = models.ManyToManyField('EssayRange')
+    year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.year}년도 논술 평가항목"
+
+
+class EssayRange(models.Model):
+    '''
+    논술의 글자 수 범위에 따른른 패널티를 저장하는 모델
+    '''
+    min_value = models.IntegerField()
+    max_value = models.IntegerField()
+    penalty = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.min} ~ {self.max} 글자: ({self.penalty}점)"
