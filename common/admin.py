d73fac393fbdad559ca2371c0cd3d80fb9272305
mongsam2 <songjaehyun121476@gmail.com>
2025-02-01 03:46:07 +0900

    feat: 다른 모델의 admin에서 상속받을 common Admin 클래스 정의

--- common/admin.py
+++ common/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import DocumentType
 
 # Register your models here.
+class CommonAdmin(admin.ModelAdmin):
+    list_display = ('student', 'document_type', 'state', 'upload_date')
+
+
+
 @admin.register(DocumentType)
 class DocumentTypeAdmin(admin.ModelAdmin):
     list_display = ('name',)  
