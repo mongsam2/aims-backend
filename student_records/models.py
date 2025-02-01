dbdc234e99110e2b3e186b895234dbf5366ef137
mongsam2 <songjaehyun121476@gmail.com>
2025-02-01 23:55:39 +0900

    chore: 생기부 모델 주석 추가

--- student_records/models.py
+++ student_records/models.py
@@ -3,9 +3,15 @@ from common.models import CommonDocument
 
 # Create your models here.
 class StudentRecord(CommonDocument):
+    '''
+    학생생활기록부를 저장하는 모델
+    '''
     summarization = models.OneToOneField('Summarization', on_delete=models.SET_NULL, null=True, blank=True, related_name='student_record')
 
 class Summarization(models.Model):
+    '''
+    생기부에서 LLM으로 요약문과 질문을 추출해 저장하는 모델
+    '''
     content = models.TextField()
     question = models.TextField()
 
