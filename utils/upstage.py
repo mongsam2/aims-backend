bf2292f34a276abc2a9c6afcca5bc067321262a0
mongsam2 <songjaehyun121476@gmail.com>
2025-02-02 02:36:17 +0900

    feat: solar llm 사용 함수 추가

--- utils/upstage.py
+++ utils/upstage.py
@@ -2,27 +2,45 @@ import requests
 from openai import OpenAI # openai==1.52.2
 
 
-def execute_ocr(api_key, file_path):
-    
-    """
-    Args:
-        api_key (str): UPSTAGE API KEY
-        file_path (str): django 데이터베이스에서 파일
+def execute_ocr(api_key, file):
 
-    Returns:
-        text: ocr에서 추출한 멀티라인 문자열 반환
-    """
-
-    filename = file_path
+    #filename = file_path
     
     url = "https://api.upstage.ai/v1/document-ai/ocr"
     headers = {"Authorization": f"Bearer {api_key}"}
     
-    files = {"document": open(filename, "rb")}
+    #files = {"document": open(filename, "rb")}
+    files = {"document": file}
     response = requests.post(url, headers=headers, files=files)
 
     if response.status_code == 200:
         content = response.json().get("text", "")
         return content
     else:
+        print(response.json())
         return "Upstage OCR API 요청에 실패했습니다."
+
+
+def get_answer_from_solar(api_key, content, prompt, temperature=0.7):
+    client = OpenAI(
+        api_key=api_key,
+        base_url="https://api.upstage.ai/v1/solar"
+    )
+
+    response = client.chat.completions.create(
+        model="solar-pro",
+        messages=[
+        {
+            "role": "system",
+            "content": prompt
+        },
+        {
+            "role": "user",
+            "content": content
+        }
+    ],
+        stream=False,
+        temperature=temperature
+    )
+
+    return response.choices[0].message.content
