208dce60f6e5dba84bccf235e32eca3475d4676d
mongsam2 <songjaehyun121476@gmail.com>
2025-02-03 03:59:44 +0900

    feat: 논술 상세조회 serializer 정의

--- essays/serializers.py
+++ essays/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
-from .models import Essay, CriteriaItem
+from .models import Essay, CriteriaItem, EssayCriteria
 
 class EssaysSerializer(ModelSerializer):
     class Meta:
@@ -19,3 +19,17 @@ class CriteriaItemSerializer(ModelSerializer):
     def get_score(self, obj):
         essay_id = self.context['essay_id']
         return obj.essay_scores.get(essay_id=essay_id).score
+
+class EssayCriteriaSerializer(ModelSerializer):
+    criteria_items = CriteriaItemSerializer(many=True)
+
+    class Meta:
+        model = EssayCriteria
+        fields = ('title', 'criteria_items')
+
+class EssayDetailSerializer(ModelSerializer):
+    criteria = EssayCriteriaSerializer()
+
+    class Meta:
+        model = Essay
+        fields = '__all__'
