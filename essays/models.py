6a0e16c4300a1cbd4bda43e27522743c3d416dcd
mongsam2 <songjaehyun121476@gmail.com>
2025-02-03 02:15:24 +0900

    fix: essays 모델들 수정

--- essays/models.py
+++ essays/models.py
@@ -6,24 +6,40 @@ class Essay(CommonDocument):
     '''
     논술 답안지를 저장하는 모델
     '''
-    evaluation = models.OneToOneField('Evaluation', on_delete=models.SET_NULL, null=True, blank=True)
+    evaluation = models.TextField(null=True, blank=True)
+    score_by_length = models.IntegerField(null=True, blank=True)
     criteria = models.ForeignKey('EssayCriteria', on_delete=models.SET_NULL, null=True, blank=True)
 
 
-class Evaluation(models.Model):
-    content = models.TextField()
+class EssayScore(models.Model):
+    '''
+    논술 답안지의 평가 항목별 점수를 저장하는 모델
+    '''
+    criteria_item = models.ForeignKey('CriteriaItem', on_delete=models.SET_NULL, null=True, blank=True)
+    essay = models.ForeignKey('Essay', on_delete=models.CASCADE, related_name='scores')
+    score = models.IntegerField()
 
 
 class EssayCriteria(models.Model):
     '''
     논술의 평가 항목과 글자 수 범위에 따른 패널티를 저장하는 모델
     '''
-    content = models.TextField()
+    title = models.CharField(max_length=100, null=True, blank=True)
     ranges = models.ManyToManyField('EssayRange')
-    year = models.IntegerField()
 
     def __str__(self):
-        return f"{self.year}년도 논술 평가항목"
+        return f"{self.title}"
+
+
+class CriteriaItem(models.Model):
+    '''
+    논술 평가 기준별, 평가 항목들을 저장하는 모델
+    '''
+    content = models.TextField()
+    essay_criteria = models.ForeignKey('EssayCriteria', on_delete=models.CASCADE, related_name='criteria_items')
+
+    def __str__(self):
+        return f"{self.content}"
 
 
 class EssayRange(models.Model):
