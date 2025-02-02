7194e98fc0e57d2631d750cb81c14cd58e4497fd
mongsam2 <songjaehyun121476@gmail.com>
2025-02-03 04:22:50 +0900

    feat: 논술 평가 기준 리스트 조회 api

--- essays/urls.py
+++ essays/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.EssaysView.as_view()),
     path('<int:id>/', views.EssayDetailView.as_view()),
+    path('criterias/', views.EssayCriteriasView.as_view()),
 ]
