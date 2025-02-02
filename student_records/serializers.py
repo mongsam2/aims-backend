ae9fd9716cb8ccbc9c3f51548dc9cf39706574fc
mongsam2 <songjaehyun121476@gmail.com>
2025-02-02 15:59:38 +0900

    feat: summarization serializer 생성, 학생부 조회 serializer에 적용

--- student_records/serializers.py
+++ student_records/serializers.py
@@ -1,13 +1,19 @@
 from rest_framework.serializers import ModelSerializer
-from .models import StudentRecord
-from students.models import Student
+from .models import StudentRecord, Summarization
 
 class StudentRecordsSerializer(ModelSerializer):
     class Meta:
         model = StudentRecord
         fields = ("id", "file",)
 
+class SummarizationSerializer(ModelSerializer):
+    class Meta:
+        model = Summarization
+        exclude = ("id",)
+
 class StudentRecordDetailSerializer(ModelSerializer):
+    summarization = SummarizationSerializer()
     class Meta:
         model = StudentRecord
         fields = "__all__"
+
