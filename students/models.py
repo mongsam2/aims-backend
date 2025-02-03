ea818dba595421e31ed104f8b2e29eb42723938d
mongsam2 <songjaehyun121476@gmail.com>
2025-02-04 08:25:05 +0900

    fix: applicantType 외래키 참조 오류 해결

--- students/models.py
+++ students/models.py
@@ -1,20 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Student(models.Model):
-    '''
-    대학에 지원한 학생들의 정보를 저장하는 모델
-    '''
-    id = models.CharField(primary_key=True, max_length=8)
-    name = models.CharField(max_length=10)
-    phone = models.CharField(max_length=11)
-    department = models.ForeignKey('Department', on_delete=models.CASCADE)
-    applicant_type = models.ForeignKey('ApplicantType', on_delete=models.CASCADE)
-    required_documents = models.ManyToManyField('common.DocumentType')
-
-    def __str__(self):
-        return f"{self.id} {self.name}"
-
 class Department(models.Model):
     '''
     대학에 있는 학과들의 정보를 담은 모델
@@ -28,7 +14,21 @@ class ApplicantType(models.Model):
     '''
     대학 지원 유형들의 정보를 담은 모델
     '''
-    name = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
+
+class Student(models.Model):
+    '''
+    대학에 지원한 학생들의 정보를 저장하는 모델
+    '''
+    id = models.CharField(primary_key=True, max_length=8)
+    name = models.CharField(max_length=10)
+    phone = models.CharField(max_length=11)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    applicant_type = models.ForeignKey(ApplicantType, on_delete=models.CASCADE, to_field='name')
+    required_documents = models.ManyToManyField('common.DocumentType')
+
+    def __str__(self):
+        return f"{self.id} {self.name}"
