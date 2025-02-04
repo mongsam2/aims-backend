5ee964134fb6abfc1f8c3b915de4f1e5594f32a1
mongsam2 <songjaehyun121476@gmail.com>
2025-02-04 09:52:44 +0900

    feat: 모든 서류 종류 조회 api

--- common/urls.py
+++ common/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ApplicantTypesView
+from .views import ApplicantTypesView, DocumentTypesView
 
 urlpatterns = [
     path('applicant-types/', ApplicantTypesView.as_view()),
+    path('document-types/', DocumentTypesView.as_view()),
 ]
