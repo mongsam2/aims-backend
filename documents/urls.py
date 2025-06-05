af3d1946bef6506d20c0cc92e3ed17b93d8b71e1
mongsam2 <songjaehyun121476@gmail.com>
2025-06-05 13:44:22 +0900

    refactor: 기존 코드 삭제

--- documents/urls.py
+++ documents/urls.py
@@ -1,8 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.DocumentUploadView.as_view()),
-    path('<int:id>/', views.DocumentUpdateView.as_view()),
-    path('passfails/<int:id>/', views.PassFailPatchView.as_view())
-]
+urlpatterns = []
