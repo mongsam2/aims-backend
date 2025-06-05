0bf3b6e917008e669f90b28f5ee139c361895c0d
mongsam2 <songjaehyun121476@gmail.com>
2025-06-05 15:39:32 +0900

    feat: aws 앱 생성

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "student_records.apps.StudentRecordsConfig",
     "documents.apps.DocumentsConfig",
     "students.apps.StudentsConfig",
+    "aws.apps.AwsConfig",
 ]
 
 MIDDLEWARE = [
@@ -151,3 +152,8 @@ REST_FRAMEWORK = {
 UPSTAGE_API_KEY = os.getenv("UPSTAGE_API_KEY")
 
 VALID_DATE = "2024-09-03"
+
+AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
+AWS_SECRET_KEY = os.environ.get("AWS_SECRET_KEY")
+AWS_BUCKET_NAME = os.environ.get("AWS_BUCKET_NAME")
+AWS_REGION_NAME = os.environ.get("AWS_REGION_NAME")
