676bb5298d007fefc4b47653f15ac9deb38d411c
mongsam2 <songjaehyun121476@gmail.com>
2025-06-05 13:44:04 +0900

    feat: 생기부 업로드 api

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v2/student-records/", include("student_records.urls")),
 ]
--- student_records/urls.py
+++ student_records/urls.py
@@ -1,9 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.StudentRecordsView.as_view()),
-    path('<int:id>/', views.StudentRecordDetailView.as_view()),
-    path('<int:id>/memo/', views.StudentRecordMemoView.as_view()),
-    path('<int:id>/scores/', views.StudentRecordScoreView.as_view()),
-]
+urlpatterns = [path("", views.StudentRecordsView.as_view())]
